992e6eb081d3a4e311066ff696eb250e06c7443e
NeoChoc <alexandra.groenert@web.de>
2022-09-13 10:58:18 +0200

    jsx and css for detail-page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,41 +1,38 @@
-import {useEffect, useState} from "react";
-import configData from "../../config.json";
-import s from "./Home.module.css";
+import { useEffect, useState } from 'react';
+import configData from '../../config.json';
+import s from './Home.module.css';
 
 const Home = () => {
+	const [popularMovies, setPopularMovies] = useState([]);
 
-    const [popularMovies, setPopularMovies] = useState([]);
+	useEffect(() => {
+		const getMovies = async () => {
+			const response = await fetch(configData['API_LINK']);
+			const data = await response.json();
+			console.log('data', data.results);
+			setPopularMovies(data.results);
+		};
+		getMovies();
+	}, []);
 
-    useEffect( () => {
-
-        const getMovies = async () => {
-            const response = await fetch(configData["API_LINK"]);
-            const data = await response.json();
-            console.log("data", data.results);
-            setPopularMovies(data.results);
-        }
-        getMovies();
-
-    }, [])
-
-
-
-    return (
-        <>
-            {<section className={s.movies}>
-            {popularMovies && popularMovies.map((movie) => {
-                console.log(`${movie.poster_path}`)
-                return(
-                    <article className={s.item}>
-                        <div>bild</div>
-                        <img src={`${configData["API_LINK"]}${movie.poster_path}`} alt=""/>
-                    </article>
-                )
-            })}
-        </section>}
-        </>
-    )
-}
-
+	return (
+		<>
+			{
+				<section className={s.movies}>
+					{popularMovies &&
+						popularMovies.map((movie) => {
+							console.log(`${movie.poster_path}`);
+							return (
+								<article className={s.item}>
+									<div>bild</div>
+									<img src={`${configData['API_LINK']}${movie.poster_path}`} alt="" />
+								</article>
+							);
+						})}
+				</section>
+			}
+		</>
+	);
+};
 
 export default Home;
