d76642db76bf95bc48b32004a08ea6f107215e64
artem <bakirovartem69@gmail.com>
2022-12-10 22:16:35 +0100

    added .github workflow

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,46 +1,45 @@
-import { NavLink, useNavigate } from 'react-router-dom';
-import { useContext } from 'react';
-import GenreContext from '../../context/context';
-
+import { NavLink, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import GenreContext from "../../context/context";
 
 const Navbar = (props) => {
-	const { setNameToSearch, nameToSearch } = useContext(GenreContext);
-	const navigate = useNavigate();
-	const path = window.location.pathname.split("/")[1];
+  const { setNameToSearch, nameToSearch } = useContext(GenreContext);
+  const navigate = useNavigate();
+  const path = window.location.hash.split("/")[1];
 
-	const changeNameToSearch = (e) => {
-		setNameToSearch(e.target.value);
-		path === "detail" && navigate("/");
-	}
+  const changeNameToSearch = (e) => {
+    console.log("path", path);
+    setNameToSearch(e.target.value);
+    path === "detail" && navigate("/");
+  };
 
-	const redirectToHome = (e) => {
-		//e.preventDefault();
-		console.log("redirect to Home");
-		setNameToSearch("");						// nach dem leeren des search muss man irgendwie nochmal ein mount erzwingen.
-		path === "detail" && navigate("/");
-	}
+  const redirectToHome = (e) => {
+    console.log("path", window.location);
+    console.log("redirect to Home");
+    setNameToSearch(""); // nach dem leeren des search muss man irgendwie nochmal ein mount erzwingen.
+    path === "detail" && navigate("/");
+  };
 
-
-	return (
-		<nav className="navigation-grid">
-			{/*Den Link am besten in ein div umbauen und dann nur den cursor: pointer dazu*/}
-			<div onClick={(e) => redirectToHome(e)} className="navigation__link" >
-				<h1 className="navigation__text--white">
-					<span className="navigation__text--color">.</span>Mov
-				</h1>
-			</div>
-			<section className="search">
-				<button className="search__button"></button>
-				<input
-					value={nameToSearch}
-					className="search__input"
-					type="text"
-					placeholder="Search something here"
-					onChange={(e) => changeNameToSearch(e)}
-				/>
-			</section>
-		</nav>
-	);
+  return (
+    <nav className="navigation-grid">
+      {/*Den Link am besten in ein div umbauen und dann nur den cursor: pointer dazu*/}
+      <div onClick={(e) => redirectToHome(e)} className="navigation__link">
+        <h1 className="navigation__text--white">
+          <span className="navigation__text--color">.</span>Mov
+        </h1>
+      </div>
+      <section className="search">
+        <button className="search__button"></button>
+        <input
+          value={nameToSearch}
+          className="search__input"
+          type="text"
+          placeholder="Search something here"
+          onChange={(e) => changeNameToSearch(e)}
+        />
+      </section>
+    </nav>
+  );
 };
 
 export default Navbar;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,164 +1,178 @@
-import { useEffect, useState, useContext } from 'react';
-import configData from '../../config.json';
-import useGetGenre from '../../hooks/useGetGenre';
+import { useEffect, useState, useContext } from "react";
+import configData from "../../config.json";
+import useGetGenre from "../../hooks/useGetGenre";
 import useRequest from "../../hooks/useRequest";
-import { v4 as uuidv4 } from 'uuid';
-import { useNavigate } from 'react-router-dom';
-import genreContext from '../../context/context';
-import GenreContext from '../../context/context';
-import no_poster from '../../assets/images/no_poster.jpg';
+import { v4 as uuidv4 } from "uuid";
+import { useNavigate } from "react-router-dom";
+import genreContext from "../../context/context";
+import GenreContext from "../../context/context";
+import no_poster from "../../assets/images/no_poster.jpg";
 
 const Home = () => {
-	const [popularMovies, setPopularMovies] = useState([]);
-	const [movieGenres, setMovieGenres] = useState([]);
-	const genres = useContext(genreContext).genresArray;
-	const { nameToSearch } = useContext(GenreContext);
-	const [searchResults, setSearchResults] = useState([]);
-	const getGenre = useGetGenre();
-	const navigate = useNavigate();
-	const request = useRequest();
+  const [popularMovies, setPopularMovies] = useState([]);
+  const [movieGenres, setMovieGenres] = useState([]);
+  const genres = useContext(genreContext).genresArray;
+  const { nameToSearch } = useContext(GenreContext);
+  const [searchResults, setSearchResults] = useState([]);
+  const getGenre = useGetGenre();
+  const navigate = useNavigate();
+  const request = useRequest();
 
-	const redirectToDetail = (e) => {
-		navigate(`/detail/${e.target.dataset.value}`);
-	};
+  const redirectToDetail = (e) => {
+    navigate(`/detail/${e.target.dataset.value}`);
+  };
 
-	useEffect(() => {
-		const getSearchResults = async () => {
-			const url = `https://api.themoviedb.org/3/search/movie?api_key=3f5bf13c3624e5013d3c11da8421e497&query=${nameToSearch}`;
-			const data = await request(url);
-			if (!data.errors) {
-				setSearchResults(data.results);
-			}
-		};
-		if (nameToSearch) {
-			getSearchResults();
-		}
+  useEffect(() => {
+    const getSearchResults = async () => {
+      const url = `https://api.themoviedb.org/3/search/movie?api_key=3f5bf13c3624e5013d3c11da8421e497&query=${nameToSearch}`;
+      const data = await request(url);
+      if (!data.errors) {
+        setSearchResults(data.results);
+      }
+    };
+    if (nameToSearch) {
+      getSearchResults();
+    }
+  }, [nameToSearch]);
 
-	}, [nameToSearch]);
+  useEffect(() => {
+    // mit length wird geprüft, dass die Daten vom fetch da sind und es keine leere Array sind.
+    if (genres.length && popularMovies.length) {
+      let genreList = getGenre(genres, popularMovies);
 
-	useEffect(() => {
-		// mit length wird geprüft, dass die Daten vom fetch da sind und es keine leere Array sind.
-		if (genres.length && popularMovies.length) {
+      setMovieGenres(genreList);
+    }
+  }, [genres, popularMovies]);
 
-			let genreList = getGenre(genres, popularMovies);
+  useEffect(() => {
+    // mit length wird geprüft, dass die Daten vom fetch da sind und es keine leere Array sind.
+    if (genres.length && searchResults.length) {
+      let genreList = getGenre(genres, searchResults);
+      setMovieGenres(genreList);
+    }
+  }, [genres, searchResults]);
 
-			setMovieGenres(genreList);
-		}
-	}, [genres, popularMovies]);
+  useEffect(() => {
+    const getMovies = async () => {
+      const data = await request(configData["API_LINK"]);
+      setPopularMovies(data.results);
+    };
+    getMovies();
+  }, [nameToSearch]);
 
-	useEffect(() => {
-		// mit length wird geprüft, dass die Daten vom fetch da sind und es keine leere Array sind.
-		if (genres.length && searchResults.length) {
-			let genreList = getGenre(genres, searchResults);
-			setMovieGenres(genreList);
+  return (
+    <>
+      {!nameToSearch && (
+        <>
+          <h2 className="home__heading">Popular Movies</h2>
+          <section className="home-grid">
+            {popularMovies &&
+              movieGenres.length &&
+              popularMovies.map((movie, idx) => {
+                return (
+                  <article className="home-item" key={uuidv4()}>
+                    <p className="home__rating">{movie.vote_average}</p>
+                    <img
+                      className="home__image"
+                      data-value={movie.id}
+                      onClick={redirectToDetail}
+                      src={
+                        movie.poster_path
+                          ? `${configData.IMG_URL}${movie.poster_path}`
+                          : no_poster
+                      }
+                      alt=""
+                    />
+                    <p className="home__text--thin">
+                      {movie.release_date.split("-")[0]} -{" "}
+                      {movieGenres[idx]?.join(", ")}
+                    </p>
+                    <h3 className="home__text--white">{movie.title}</h3>
+                  </article>
+                );
+              })}
+          </section>
+        </>
+      )}
 
-		}
+      {nameToSearch && (
+        <section>
+          {searchResults.length && (
+            <h2 className="home-search__heading">Search Results</h2>
+          )}
+          {!searchResults.length && nameToSearch !== "Supercode" && (
+            <h2 className="home-search__heading">No Search Results</h2>
+          )}
+          {nameToSearch && searchResults && (
+            <section className="home-search-grid">
+              {searchResults.map((movie, idx) => {
+                return (
+                  <article className="home-search-item" key={uuidv4()}>
+                    <p className="home-search__rating">{movie?.vote_average}</p>
+                    <img
+                      className="home-search__image"
+                      data-value={movie?.id}
+                      onClick={redirectToDetail}
+                      src={
+                        movie.poster_path
+                          ? `${configData.IMG_URL}${movie.poster_path}`
+                          : no_poster
+                      }
+                      alt=""
+                    />
+                    <p className="home-search__text--thin">
+                      {movie.release_date?.split("-")[0]} -{" "}
+                      {movieGenres[idx]?.join(", ")}
+                    </p>
+                    <h3 className="home-search__text--white">{movie?.title}</h3>
+                  </article>
+                );
+              })}
+            </section>
+          )}
 
-	}, [genres, searchResults]);
-
-	useEffect(() => {
-		const getMovies = async () => {
-			const data = await request(configData['API_LINK']);
-			setPopularMovies(data.results);
-		};
-		getMovies();
-	}, [nameToSearch]);
-
-	return (
-		<>
-			{!nameToSearch &&
-				<>
-					<h2 className="home__heading">Popular Movies</h2>
-					<section className="home-grid">
-						{popularMovies &&
-							movieGenres.length &&
-							popularMovies.map((movie, idx) => {
-								return (
-									<article className="home-item" key={uuidv4()}>
-										<p className="home__rating">{movie.vote_average}</p>
-										<img
-											className="home__image"
-											data-value={movie.id}
-											onClick={redirectToDetail}
-											src={movie.poster_path ? `${configData.IMG_URL}${movie.poster_path}` : no_poster}
-											alt=""
-										/>
-										<p className="home__text--thin">
-											{movie.release_date.split('-')[0]} - {movieGenres[idx]?.join(', ')}
-										</p>
-										<h3 className="home__text--white">{movie.title}</h3>
-									</article>
-								);
-							})}
-					</section>
-				</>}
-
-
-			{nameToSearch && (
-				<section>
-					{searchResults.length && <h2 className="home-search__heading">Search Results</h2>}
-					{!searchResults.length && nameToSearch !== "Supercode" && <h2 className="home-search__heading">No Search Results</h2>}
-					{nameToSearch && searchResults && (
-						<section className="home-search-grid">
-							{searchResults.map((movie, idx) => {
-								return (
-									<article className="home-search-item" key={uuidv4()}>
-										<p className="home-search__rating">{movie?.vote_average}</p>
-										<img
-											className="home-search__image"
-											data-value={movie?.id}
-											onClick={redirectToDetail}
-											src={movie.poster_path ? `${configData.IMG_URL}${movie.poster_path}` : no_poster}
-											alt=""
-										/>
-										<p className="home-search__text--thin">
-											{movie.release_date?.split('-')[0]} - {movieGenres[idx]?.join(', ')}
-										</p>
-										<h3 className="home-search__text--white">{movie?.title}</h3>
-									</article>
-								);
-							})}
-						</section>
-					)}
-
-					{nameToSearch && searchResults && !searchResults.length && nameToSearch !== "Supercode" && (
-						<article className="search-nores" key={uuidv4()}>
-							<h3 className="search-nores__heading">Entschuldigung! </h3>
-							<p className="search-nores__text">
-								Leider haben wir keinen Film, der <strong>{nameToSearch}</strong> heißt. Falls es diesen Film
-								wirklich gibt, schreiben sie uns gerne eine Email.
-							</p>
-							<p className="home-search__text--thin"></p>
-							<a className="search-nores__link" href="mailto:FilmDatenBank@fakeEmailBitteNixSenden.com">
-								Email Us
-							</a>
-						</article>
-					)}
-				</section>
-			)}
-			{nameToSearch === "Supercode" &&
-				<div>
-					<h2 className="home-search__heading">Search Results</h2>
-						<section className="home-search-grid">
-									<article className="home-search-item">
-										<p className="home-search__rating">100</p>
-										<img
-											className="home-search__image"
-											onClick={redirectToDetail}
-											src={no_poster}
-											data-value="Supercode"
-											alt=""
-										/>
-										<p className="home-search__text--thin">
-											2022
-										</p>
-										<h3>The bizarre Adventure of Supercoders</h3>
-									</article>
-						</section>
-				</div>
-
-			}
-		</>
-	);
+          {nameToSearch &&
+            searchResults &&
+            !searchResults.length &&
+            nameToSearch !== "Supercode" && (
+              <article className="search-nores" key={uuidv4()}>
+                <h3 className="search-nores__heading">Entschuldigung! </h3>
+                <p className="search-nores__text">
+                  Leider haben wir keinen Film, der{" "}
+                  <strong>{nameToSearch}</strong> heißt. Falls es diesen Film
+                  wirklich gibt, schreiben sie uns gerne eine Email.
+                </p>
+                <p className="home-search__text--thin"></p>
+                <a
+                  className="search-nores__link"
+                  href="mailto:FilmDatenBank@fakeEmailBitteNixSenden.com"
+                >
+                  Email Us
+                </a>
+              </article>
+            )}
+        </section>
+      )}
+      {nameToSearch === "Supercode" && (
+        <div>
+          <h2 className="home-search__heading">Search Results</h2>
+          <section className="home-search-grid">
+            <article className="home-search-item">
+              <p className="home-search__rating">100</p>
+              <img
+                className="home-search__image"
+                onClick={redirectToDetail}
+                src={no_poster}
+                data-value="Supercode"
+                alt=""
+              />
+              <p className="home-search__text--thin">2022</p>
+              <h3>The bizarre Adventure of Supercoders</h3>
+            </article>
+          </section>
+        </div>
+      )}
+    </>
+  );
 };
 export default Home;
